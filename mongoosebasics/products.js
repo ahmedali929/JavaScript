707bd55c49a080e66925410a1fab0b248951ff07
Ahmed Ali <ahmed.ali3509@gmail.com>
2025-02-12 22:00:16 -0600

    added instance methods to schema models

--- mongoosebasics/products.js
+++ mongoosebasics/products.js
@@ -41,25 +41,51 @@ const productSchema = new mongoose.Schema({
     }
 });
 
+productSchema.methods.greet = function() {
+    console.log("HELLO! HI! HOWDY!");
+    console.log(`-  from ${this.name}`);
+}
+
+productSchema.methods.toggleOnSale = function() {
+    this.onSale = !this.onSale;
+    return this.save();
+}
+
+productSchema.methods.addCategory = function(newCat) {
+    this.categories.push(newCat);
+    return this.save();
+}
+
 const Product = mongoose.model('Product', productSchema);
 
-const bike = new Product({name:'Mountain Bike', price: 599.99});
-const bikeHelmet = new Product({name:'Bike Helmet', price: 29.99, categories: ['Cycling', 'Safety'], size: 'L'});
-Product.findOneAndUpdate({name: 'Bike Helmet'}, {price: 19.99}, {new: true, runValidators: true})
-    .then(data => {
-        console.log("IT WORKED")
-        console.log(data);
-    })
-    .catch(err => {
-        console.log("OH NO ERROR")
-        console.log(err)
-    })
-bikeHelmet.save()
-    .then(data => {
-        console.log("IT WORKED")
-        console.log(data);
-    })
-    .catch(err => {
-        console.log("OH NO ERROR")
-        console.log(err)
-    })
+const findProduct = async () => {
+    const foundProduct = await Product.findOne({name: 'Bike Helmet'});
+    console.log(foundProduct);
+    await foundProduct.toggleOnSale();
+    console.log(foundProduct);
+    await foundProduct.addCategory('Apparel');
+    console.log(foundProduct);
+}
+
+findProduct();
+
+// const bike = new Product({name:'Mountain Bike', price: 599.99});
+// const bikeHelmet = new Product({name:'Bike Helmet', price: 29.99, categories: ['Cycling', 'Safety'], size: 'L'});
+// Product.findOneAndUpdate({name: 'Bike Helmet'}, {price: 19.99}, {new: true, runValidators: true})
+//     .then(data => {
+//         console.log("IT WORKED")
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.log("OH NO ERROR")
+//         console.log(err)
+//     })
+// bikeHelmet.save()
+//     .then(data => {
+//         console.log("IT WORKED")
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.log("OH NO ERROR")
+//         console.log(err)
+//     })
