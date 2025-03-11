ce338678ba461f576101404ff09a82b2babf23a2
Ahmed Ali <ahmed.ali3509@gmail.com>
2025-03-11 12:24:24 -0500

    adding functionality to add new products and also added a Products Details page

--- mongooseExpress/index.js
+++ mongooseExpress/index.js
@@ -22,6 +22,13 @@ app.get('/products', async (req, res) => {
     res.render('products/index', { products });
 })
 
+app.get('/products/:id' async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    console.log(product);
+    res.render('products/show');
+})
+
 
 
 app.listen(3000, () => {
