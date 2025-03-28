118f5e6d3c0c68ac203cb4c866d1a6e46bf56053
Ahmed Ali <ahmed.ali3509@gmail.com>
2025-03-28 00:34:07 -0500

    adding more functionality regarding the farm and product connection

--- DemoApp/index.js
+++ DemoApp/index.js
@@ -34,7 +34,7 @@ app.get('/farms/new', (req, res) => {
 })
 
 app.get('/farms/:id', async(req, res) => {
-    const farm = await Farm.findById(req.params.id)
+    const farm = await Farm.findById(req.params.id).populate('products');
     res.render('farms/show', {farm})
 })
 
@@ -44,9 +44,10 @@ app.post('/farms', async (req, res) => {
     res.redirect('/farms');
 })
 
-app.get('/farms/:id/products/new', (req, res) => {
+app.get('/farms/:id/products/new', async (req, res) => {
     const { id } = req.params;
-    res.render('products/new', { categories, id })
+    const farm = await Farm.findById(id);
+    res.render('products/new', { categories, farm })
 })
 
 app.post('/farms/:id/products', async(req, res) => {
@@ -58,7 +59,7 @@ app.post('/farms/:id/products', async(req, res) => {
     farm.products.push(product);
     await farm.save();
     await product.save();
-    res.send(farm);
+    res.redirect(`/farms/${id}`)
 })
 
 // PRODUCT ROUTES
@@ -89,7 +90,7 @@ app.post('/products', async (req, res) => {
 
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
-    const product = await Product.findById(id);
+    const product = await Product.findById(id).populate('farm', 'name');
     res.render('products/show', { product });
 })
 
