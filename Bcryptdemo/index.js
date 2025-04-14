062e1107dab7d2ba0244b5fc65ebdebb024ec200
Ahmed Ali <ahmed.ali3509@gmail.com>
2025-04-13 22:05:33 -0500

    adding a bcrypt demo

---
+++ Bcryptdemo/index.js
@@ -0,0 +1,19 @@
+const bcrypt = require('bcrypt'); 
+
+const hashPassword = async (pw) => {
+    const salt = await bcrypt.genSalt(12);
+    const hash = await bcrypt.hash(pw, salt);
+    console.log(salt);
+    console.log(hash);
+}
+
+const login = async(pw, hashedPw) => {
+    const result = await bcrypt.compare(pw, hashedPw);
+    if (result) {
+        console.log("logged you in! successful match")
+    } else {
+        console.log("incorrect")
+    }
+} 
+
+hashPassword('monkey');
