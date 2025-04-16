15756371cad6e314587f64dc5471ecdd8ba45e45
Ahmed Ali <ahmed.ali3509@gmail.com>
2025-04-16 17:14:41 -0500

    created middleware that requires login. this replaces the more explicit login functionality that we had

--- AuthDemo/index.js
+++ AuthDemo/index.js
@@ -20,6 +20,13 @@ app.set('views', 'views');
 app.use(express.urlencoded({extended:true}));
 app.use(session({ secret: 'notagoodsecret'}));
 
+const requireLogin = (req, res, next) => {
+    if(!req.session.user_id) {
+        return res.redirect('/login')
+    }
+    next();
+}
+
 app.get('/', (req, res) => {
     res.send('This is the home page');
 })
@@ -61,11 +68,12 @@ app.post('/logout', (req,res) => {
     res.redirect('/login');
 })
 
-app.get('/secret', (req, res) => {
-    if (!req.session.user_id) {
-        return res.redirect('/login');
-    }
+app.get('/secret', requireLogin, (req, res) => {
     res.render('secret');
+})
+
+app.get('/topsecret', requireLogin, (req, res) => {
+    res.send("Top Secret!")
 })
 
 app.listen(3000, () => {
