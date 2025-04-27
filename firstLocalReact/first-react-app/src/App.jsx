501f533a16ba93ad45144e9bf28a595ca5916c2d
Ahmed Ali <ahmed.ali3509@gmail.com>
2025-04-26 22:27:16 -0500

    recreated first react app

---
+++ firstLocalReact/first-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Chicken from "./Chicken";
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Chicken />
+      <Chicken />
+      <Chicken />
+    </div>
+  )
+  
+}
+
+export default App
